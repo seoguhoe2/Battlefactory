7b4e91e8e667a71e3f14601a83dee89c6ca603e1
seoguhoe2 <js8337349@naver.com>
2025-10-02 12:40:18 +0900

    Develop 시작

---
+++ Battlefactory/src/main/java/com/example/battlefactory/BattlefactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.battlefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlefactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BattlefactoryApplication.class, args);
+    }
+
+}
---
+++ Battlefactory/src/test/java/com/example/battlefactory/BattlefactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.battlefactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlefactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
